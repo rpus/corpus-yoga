872b5f36b3ff778a4d1fd44b7bf4331e59a1a4c8
rpus <rpus@users.noreply.github.comm>
2026-04-21 21:21:32 +0100

    fix: Update variable name in pre-commit hook for correct path resolution

--- src/test/pre_commit.sh
+++ src/test/pre_commit.sh
@@ -4,11 +4,11 @@
 #   chmod +x .git/hooks/pre-commit
 
 set -euo pipefail
-GIT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+GIT_HOOKS_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 
 # shellcheck source=/dev/null
 source ~/venvs/general/bin/activate
-python "$GIT_DIR/../../src/test/pre_commit.py"
+python "$GIT_HOOKS_DIR/../../src/test/pre_commit.py"
 exit_code=$?
 deactivate
 exit $exit_code
